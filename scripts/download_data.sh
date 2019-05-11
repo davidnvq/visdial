6cd3d0b0c97943bc8387ac5b4fbac0969677b726
Quang-Mac <quangdtsc@gmail.com>
2019-05-12 01:15:56 +0900

    edit download file

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -11,14 +11,15 @@ function download_file (){
     fi
 }
 
+DATASET=/content/datasets
 
-FILE[1]='/content/datasets/features_faster_rcnn_x101_train.h5'
-FILE[2]='/content/datasets/features_faster_rcnn_x101_val.h5'
-FILE[3]='/content/datasets/features_faster_rcnn_x101_test.h5'
-FILE[4]='/content/datasets/visdial_1.0_word_counts_train.json'
-FILE[5]='/content/datasets/visdial_1.0_train.zip'
-FILE[6]='/content/datasets/visdial_1.0_val.zip'
-FILE[7]='/content/datasets/visdial_1.0_val_dense_annotations.json'
+FILE[1]=$DATASET/features_faster_rcnn_x101_train.h5
+FILE[2]=$DATASET/features_faster_rcnn_x101_val.h5
+FILE[3]=$DATASET/features_faster_rcnn_x101_test.h5
+FILE[4]=$DATASET/visdial_1.0_word_counts_train.json
+FILE[5]=$DATASET/visdial_1.0_train.zip
+FILE[6]=$DATASET/visdial_1.0_val.zip
+FILE[7]=$DATASET/visdial_1.0_val_dense_annotations.json
 
 LINK[1]='https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_train.h5'
 LINK[2]='https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_val.h5'
@@ -27,6 +28,13 @@ LINK[4]='https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/visdial_1.0_word_
 LINK[5]='https://www.dropbox.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip?dl=0'
 LINK[6]='https://www.dropbox.com/s/ibs3a0zhw74zisc/visdial_1.0_val.zip?dl=0'
 LINK[7]='https://www.dropbox.com/s/3knyk09ko4xekmc/visdial_1.0_val_dense_annotations.json\?dl=0'
+
+
+# check directories
+if [ ! -d $DATASET ]; then
+    mkdir -p $DATASET
+fi
+
 
 # Download
 for i in {1..20}
