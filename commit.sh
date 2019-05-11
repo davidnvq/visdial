0794b1ccd07f55b24d82fc6389145ebba0d06351
Quang-Mac <quangdtsc@gmail.com>
2019-05-12 02:41:15 +0900

    colab

---
+++ commit.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd ~/Dropbox/repos/visdial
+git add .
+git commit -m "colab"
+git push -u origin master
--- train.py
+++ train.py
@@ -184,20 +184,20 @@ is_abtoks = True if config["model"]["decoder"] != "disc" else False
 is_return = True if config["model"]["decoder"] == "disc" else False
 
 # TODO: Comment this overfit test
-train_dataset = VisDialDataset(
-		config["dataset"],
-		args.json_train,
-		overfit=args.overfit,
-		in_memory=args.in_memory,
-		return_options=is_return,
-		add_boundary_toks=is_abtoks,
-		)
-train_dataloader = DataLoader(
-		train_dataset,
-		batch_size=config["solver"]["batch_size"],
-		num_workers=args.cpu_workers,
-		shuffle=True,
-		)
+# train_dataset = VisDialDataset(
+# 		config["dataset"],
+# 		args.json_train,
+# 		overfit=args.overfit,
+# 		in_memory=args.in_memory,
+# 		return_options=is_return,
+# 		add_boundary_toks=is_abtoks,
+# 		)
+# train_dataloader = DataLoader(
+# 		train_dataset,
+# 		batch_size=config["solver"]["batch_size"],
+# 		num_workers=args.cpu_workers,
+# 		shuffle=True,
+# 		)
 
 val_dataset = VisDialDataset(
 		config["dataset"],
@@ -218,194 +218,194 @@ val_dataloader = DataLoader(
 		worker_init_fn=init_fn
 		)
 
-# TODO: Uncomment this overfit test
-# train_dataset = val_dataset
-# train_dataloader = val_dataloader
-
-# Pass vocabulary to construct Embedding layer.
-encoder = AttentionEncoder(config["model"], train_dataset.vocabulary)
-decoder = Decoder(config["model"], train_dataset.vocabulary)
-
-print("Encoder: {}".format(config["model"]["encoder"]))
-print("Decoder: {}".format(config["model"]["decoder"]))
-
-# Share word embedding between encoder and decoder.
-decoder.word_embed = encoder.word_embed
-
-is_bilstm = config['model']['is_bilstm']
-
-# Wrap encoder and decoder in a model.
-model = EncoderDecoderModel(encoder, decoder, is_bilstm=is_bilstm)
-
-model = model.to(device)
-
-if len(args.gpu_ids) > 1:
-	model = nn.DataParallel(model)
-
-
-# Loss function.
-if config["model"]["decoder"] == "disc":
-	criterion = nn.CrossEntropyLoss()
-elif config["model"]["decoder"] == "gen":
-	criterion = nn.CrossEntropyLoss(
-			ignore_index=train_dataset.vocabulary.PAD_INDEX
-			)
-else:
-	raise NotImplementedError
-
-if config["solver"]["training_splits"] == "trainval":
-	iterations = (len(train_dataset) + len(val_dataset)) // config["solver"][
-		"batch_size"
-	] + 1
-	num_examples = torch.tensor(len(train_dataset) + len(val_dataset), dtype=torch.float)
-else:
-	iterations = len(train_dataset) // config["solver"]["batch_size"] + 1
-	num_examples = torch.tensor(len(train_dataset), dtype=torch.float)
-
-optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
-# =============================================================================
-#   SETUP BEFORE TRAINING LOOP
-# =============================================================================
-summary_writer = SummaryWriter(log_dir=args.save_dirpath)
-checkpoint_manager = CheckpointManager(model, optimizer, args.save_dirpath, config=config)
-sparse_metrics = SparseGTMetrics()
-ndcg = NDCG()
-monitor = Monitor(val_dataset, save_path=args.monitor_path)
-
-# If loading from checkpoint, adjust start epoch and load parameters.
-if args.load_pthpath == "":
-	start_epoch = 0
-else:
-	# "path/to/checkpoint_xx.pth" -> xx
-	start_epoch = int(args.load_pthpath.split("_")[-1][:-4]) + 1
-
-	model_state_dict, optimizer_state_dict = load_checkpoint(args.load_pthpath)
-	if isinstance(model, nn.DataParallel):
-		model.module.load_state_dict(model_state_dict)
-	else:
-		model.load_state_dict(model_state_dict)
-	optimizer.load_state_dict(optimizer_state_dict)
-	print("Loaded model from {}".format(args.load_pthpath))
-
-# =============================================================================
-#   TRAINING LOOP
-# =============================================================================
-# Forever increasing counter to keep track of iterations (for tensorboard log).
-global_iteration_step = start_epoch * iterations
-
-def move_to_cuda(batch, device):
-	for key in batch:
-		batch[key] = batch[key].to(device)
-
-for epoch in range(start_epoch, config["solver"]["num_epochs"]):
-	# -------------------------------------------------------------------------
-	#   ON EPOCH START  (combine dataloaders if training on train + val)
-	# -------------------------------------------------------------------------
-	if config["solver"]["training_splits"] == "trainval":
-		combined_dataloader = itertools.chain(train_dataloader, val_dataloader)
-	else:
-		combined_dataloader = itertools.chain(train_dataloader)
-
-	print(f"\nTraining for epoch {epoch}:")
-
-	with tqdm(total=iterations) as pbar:
-		epoch_loss = torch.tensor(0.0)
-
-		for i, batch in enumerate(combined_dataloader):
-			move_to_cuda(batch, device)
-
-			# zero grad
-			optimizer.zero_grad()
-
-			# do forward
-			output = model(batch)
-
-			# get target
-			if config["model"]["decoder"] == "disc":
-				target = batch['ans_ind']
-				sparse_metrics.observe(output, target)
-			else:
-				target = batch["ans_out"]
-
-			# compute loss
-			batch_loss = criterion(output.view(-1, output.size(-1)), target.view(-1))
-
-			# compute gradients
-			batch_loss.backward()
-
-			# update params
-			optimizer.step()
-
-			pbar.set_postfix(epoch=epoch, batch_loss=batch_loss.item())
-			pbar.update(1)
-
-			experiment.log_metric('train/batch_loss', batch_loss.item())
-			summary_writer.add_scalar("train/batch_loss", batch_loss, global_iteration_step)
-			summary_writer.add_scalar("train/lr", optimizer.param_groups[0]["lr"], global_iteration_step)
-
-			global_iteration_step += 1
-			torch.cuda.empty_cache()
-
-
-			epoch_loss += batch["ques"].size(0) * batch_loss.detach()
-
-		if config["model"]["decoder"] == "disc":
-			all_metrics = {}
-			all_metrics.update(sparse_metrics.retrieve(reset=True))
-
-			for metric_name, metric_value in all_metrics.items():
-				print('')
-				print(f"{metric_name}: {metric_value}")
-				experiment.log_metric(f"train/{metric_name}", metric_value)
-
-			summary_writer.add_scalars("train/metrics", all_metrics, global_iteration_step)
-
-		epoch_loss /= num_examples
-
-		summary_writer.add_scalar("train/epoch_loss", epoch_loss, i)
-		experiment.log_metric('train/epoch_loss', epoch_loss.item())
-
-	# -------------------------------------------------------------------------
-	#   ON EPOCH END  (checkpointing and validation)
-	# -------------------------------------------------------------------------
-	checkpoint_manager.step(epoch=epoch)
-
-	# Validate and report automatic metrics.
-	if args.validate:
-
-		# Switch dropout, batchnorm etc to the correct mode.
-		model.eval()
-
-		epoch_loss = torch.tensor(0.0)
-
-		print(f"\nValidation after epoch {epoch}:")
-
-		for i, batch in enumerate(tqdm(val_dataloader)):
-
-			move_to_cuda(batch, device)
-
-			with torch.no_grad():
-				output = model(batch)
-				sparse_metrics.observe(output, batch["ans_ind"])
-
-				if "gt_relevance" in batch:
-					output = output[:, batch["round_id"] - 1, :]
-					ndcg.observe(output, batch["gt_relevance"])
-					monitor.update(batch['img_ids'], output, batch['ans_ind'])
-
-		if 'gt_relevance' in batch:
-			monitor.export()
-
-		all_metrics = {}
-		all_metrics.update(sparse_metrics.retrieve(reset=True))
-		all_metrics.update(ndcg.retrieve(reset=True))
-
-		for metric_name, metric_value in all_metrics.items():
-			print(f"{metric_name}: {metric_value}")
-			experiment.log_metric(f"val/{metric_name}", metric_value)
-
-		summary_writer.add_scalars("val/metrics", all_metrics, global_iteration_step)
-
-		model.train()
-		torch.cuda.empty_cache()
+# # TODO: Uncomment this overfit test
+# # train_dataset = val_dataset
+# # train_dataloader = val_dataloader
+#
+# # Pass vocabulary to construct Embedding layer.
+# encoder = AttentionEncoder(config["model"], train_dataset.vocabulary)
+# decoder = Decoder(config["model"], train_dataset.vocabulary)
+#
+# print("Encoder: {}".format(config["model"]["encoder"]))
+# print("Decoder: {}".format(config["model"]["decoder"]))
+#
+# # Share word embedding between encoder and decoder.
+# decoder.word_embed = encoder.word_embed
+#
+# is_bilstm = config['model']['is_bilstm']
+#
+# # Wrap encoder and decoder in a model.
+# model = EncoderDecoderModel(encoder, decoder, is_bilstm=is_bilstm)
+#
+# model = model.to(device)
+#
+# if len(args.gpu_ids) > 1:
+# 	model = nn.DataParallel(model)
+#
+#
+# # Loss function.
+# if config["model"]["decoder"] == "disc":
+# 	criterion = nn.CrossEntropyLoss()
+# elif config["model"]["decoder"] == "gen":
+# 	criterion = nn.CrossEntropyLoss(
+# 			ignore_index=train_dataset.vocabulary.PAD_INDEX
+# 			)
+# else:
+# 	raise NotImplementedError
+#
+# if config["solver"]["training_splits"] == "trainval":
+# 	iterations = (len(train_dataset) + len(val_dataset)) // config["solver"][
+# 		"batch_size"
+# 	] + 1
+# 	num_examples = torch.tensor(len(train_dataset) + len(val_dataset), dtype=torch.float)
+# else:
+# 	iterations = len(train_dataset) // config["solver"]["batch_size"] + 1
+# 	num_examples = torch.tensor(len(train_dataset), dtype=torch.float)
+#
+# optimizer = optim.Adam(model.parameters(), lr=args.lr)
+#
+# # =============================================================================
+# #   SETUP BEFORE TRAINING LOOP
+# # =============================================================================
+# summary_writer = SummaryWriter(log_dir=args.save_dirpath)
+# checkpoint_manager = CheckpointManager(model, optimizer, args.save_dirpath, config=config)
+# sparse_metrics = SparseGTMetrics()
+# ndcg = NDCG()
+# monitor = Monitor(val_dataset, save_path=args.monitor_path)
+#
+# # If loading from checkpoint, adjust start epoch and load parameters.
+# if args.load_pthpath == "":
+# 	start_epoch = 0
+# else:
+# 	# "path/to/checkpoint_xx.pth" -> xx
+# 	start_epoch = int(args.load_pthpath.split("_")[-1][:-4]) + 1
+#
+# 	model_state_dict, optimizer_state_dict = load_checkpoint(args.load_pthpath)
+# 	if isinstance(model, nn.DataParallel):
+# 		model.module.load_state_dict(model_state_dict)
+# 	else:
+# 		model.load_state_dict(model_state_dict)
+# 	optimizer.load_state_dict(optimizer_state_dict)
+# 	print("Loaded model from {}".format(args.load_pthpath))
+#
+# # =============================================================================
+# #   TRAINING LOOP
+# # =============================================================================
+# # Forever increasing counter to keep track of iterations (for tensorboard log).
+# global_iteration_step = start_epoch * iterations
+#
+# def move_to_cuda(batch, device):
+# 	for key in batch:
+# 		batch[key] = batch[key].to(device)
+#
+# for epoch in range(start_epoch, config["solver"]["num_epochs"]):
+# 	# -------------------------------------------------------------------------
+# 	#   ON EPOCH START  (combine dataloaders if training on train + val)
+# 	# -------------------------------------------------------------------------
+# 	if config["solver"]["training_splits"] == "trainval":
+# 		combined_dataloader = itertools.chain(train_dataloader, val_dataloader)
+# 	else:
+# 		combined_dataloader = itertools.chain(train_dataloader)
+#
+# 	print(f"\nTraining for epoch {epoch}:")
+#
+# 	with tqdm(total=iterations) as pbar:
+# 		epoch_loss = torch.tensor(0.0)
+#
+# 		for i, batch in enumerate(combined_dataloader):
+# 			move_to_cuda(batch, device)
+#
+# 			# zero grad
+# 			optimizer.zero_grad()
+#
+# 			# do forward
+# 			output = model(batch)
+#
+# 			# get target
+# 			if config["model"]["decoder"] == "disc":
+# 				target = batch['ans_ind']
+# 				sparse_metrics.observe(output, target)
+# 			else:
+# 				target = batch["ans_out"]
+#
+# 			# compute loss
+# 			batch_loss = criterion(output.view(-1, output.size(-1)), target.view(-1))
+#
+# 			# compute gradients
+# 			batch_loss.backward()
+#
+# 			# update params
+# 			optimizer.step()
+#
+# 			pbar.set_postfix(epoch=epoch, batch_loss=batch_loss.item())
+# 			pbar.update(1)
+#
+# 			experiment.log_metric('train/batch_loss', batch_loss.item())
+# 			summary_writer.add_scalar("train/batch_loss", batch_loss, global_iteration_step)
+# 			summary_writer.add_scalar("train/lr", optimizer.param_groups[0]["lr"], global_iteration_step)
+#
+# 			global_iteration_step += 1
+# 			torch.cuda.empty_cache()
+#
+#
+# 			epoch_loss += batch["ques"].size(0) * batch_loss.detach()
+#
+# 		if config["model"]["decoder"] == "disc":
+# 			all_metrics = {}
+# 			all_metrics.update(sparse_metrics.retrieve(reset=True))
+#
+# 			for metric_name, metric_value in all_metrics.items():
+# 				print('')
+# 				print(f"{metric_name}: {metric_value}")
+# 				experiment.log_metric(f"train/{metric_name}", metric_value)
+#
+# 			summary_writer.add_scalars("train/metrics", all_metrics, global_iteration_step)
+#
+# 		epoch_loss /= num_examples
+#
+# 		summary_writer.add_scalar("train/epoch_loss", epoch_loss, i)
+# 		experiment.log_metric('train/epoch_loss', epoch_loss.item())
+#
+# 	# -------------------------------------------------------------------------
+# 	#   ON EPOCH END  (checkpointing and validation)
+# 	# -------------------------------------------------------------------------
+# 	checkpoint_manager.step(epoch=epoch)
+#
+# 	# Validate and report automatic metrics.
+# 	if args.validate:
+#
+# 		# Switch dropout, batchnorm etc to the correct mode.
+# 		model.eval()
+#
+# 		epoch_loss = torch.tensor(0.0)
+#
+# 		print(f"\nValidation after epoch {epoch}:")
+#
+# 		for i, batch in enumerate(tqdm(val_dataloader)):
+#
+# 			move_to_cuda(batch, device)
+#
+# 			with torch.no_grad():
+# 				output = model(batch)
+# 				sparse_metrics.observe(output, batch["ans_ind"])
+#
+# 				if "gt_relevance" in batch:
+# 					output = output[:, batch["round_id"] - 1, :]
+# 					ndcg.observe(output, batch["gt_relevance"])
+# 					monitor.update(batch['img_ids'], output, batch['ans_ind'])
+#
+# 		if 'gt_relevance' in batch:
+# 			monitor.export()
+#
+# 		all_metrics = {}
+# 		all_metrics.update(sparse_metrics.retrieve(reset=True))
+# 		all_metrics.update(ndcg.retrieve(reset=True))
+#
+# 		for metric_name, metric_value in all_metrics.items():
+# 			print(f"{metric_name}: {metric_value}")
+# 			experiment.log_metric(f"val/{metric_name}", metric_value)
+#
+# 		summary_writer.add_scalars("val/metrics", all_metrics, global_iteration_step)
+#
+# 		model.train()
+# 		torch.cuda.empty_cache()
