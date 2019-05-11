6c7eba09e7eff506d3ad390defec670de789815f
Quang-Mac <quangdtsc@gmail.com>
2019-05-12 05:39:42 +0900

    colab

--- visdial/data/dataset.py
+++ visdial/data/dataset.py
@@ -48,9 +48,9 @@ class VisDialDataset(Dataset):
 		self.vocabulary = Vocabulary(jsonpath_vocab_dict, config["vocab_min_count"])
 		print("done loading vocab")
 
-		# self.hdf_reader = ImageFeaturesHdfReader(hdfpath_img_features)
-		print("done loading image reader!")
+		self.hdf_reader = ImageFeaturesHdfReader(hdfpath_img_features)
 
+		print("done loading image reader!")
 		# Keep a list of image_ids as primary keys to access data.
 		self.image_ids = list(self.dialogs_reader.dialogs.keys())
 
