0b7f496a92c438dbbb2c5d5785eb7def72a52c57
Quang-Mac <quangdtsc@gmail.com>
2019-05-12 02:55:45 +0900

    colab

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -16,19 +16,19 @@ FILE[1]=$DATASET/features_faster_rcnn_x101_train.h5
 FILE[2]=$DATASET/features_faster_rcnn_x101_val.h5
 FILE[3]=$DATASET/features_faster_rcnn_x101_test.h5
 FILE[4]=$DATASET/visdial_1.0_word_counts_train.json
-FILE[5]=$DATASET/visdial_1.0_train.zip
-FILE[6]=$DATASET/visdial_1.0_val.zip
-FILE[7]=$DATASET/visdial_1.0_val_dense_annotations.json
-FILE[8]=$DATASET/visdial_1.0_val.json
-FILE[9]=$DATASET/visdial_1.0_train.json
+FILE[5]=$DATASET/visdial_1.0_val_dense_annotations.json
+FILE[6]=$DATASET/visdial_1.0_train.zip
+FILE[7]=$DATASET/visdial_1.0_val.zip
+FILE[8]=$DATASET/visdial_1.0_train.json
+FILE[9]=$DATASET/visdial_1.0_val.json
 
 LINK[1]='https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_train.h5'
 LINK[2]='https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_val.h5'
 LINK[3]='https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_test.h5'
 LINK[4]='https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/visdial_1.0_word_counts_train.json'
-LINK[5]='https://www.dropbox.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip?dl=0'
-LINK[6]='https://www.dropbox.com/s/ibs3a0zhw74zisc/visdial_1.0_val.zip?dl=0'
-LINK[7]='https://www.dropbox.com/s/3knyk09ko4xekmc/visdial_1.0_val_dense_annotations.json?dl=0'
+LINK[5]='https://www.dropbox.com/s/3knyk09ko4xekmc/visdial_1.0_val_dense_annotations.json?dl=0'
+LINK[6]='https://www.dropbox.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip?dl=0'
+LINK[7]='https://www.dropbox.com/s/ibs3a0zhw74zisc/visdial_1.0_val.zip?dl=0'
 
 
 # check directories
@@ -46,6 +46,7 @@ done
 for i in {8..9}
 do
     if [ ! -f ${FILE[i]} ]; then
-        unzip ${FILE[i]} -d $DATASET
+        idx=$((i - 2))
+        unzip ${FILE[idx]} -d $DATASET
     fi
 done
