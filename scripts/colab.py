64542e0bcd01d12e2a4bd2a6459646fb3af51be4
Quang-Mac <quangdtsc@gmail.com>
2019-05-15 15:35:34 +0200

    colab

--- scripts/colab.py
+++ scripts/colab.py
@@ -41,36 +41,42 @@ def file_exists(file_path):
 def download_dataset(train=False):
 	dir_path = os.path.join(ROOT, 'datasets', 'visdial')
 	file_paths = [
-		os.path.join(dir_path, 'features_faster_rcnn_x101_train.h5'),
 		os.path.join(dir_path, 'features_faster_rcnn_x101_val.h5'),
 		os.path.join(dir_path, 'features_faster_rcnn_x101_test.h5'),
 		os.path.join(dir_path, 'visdial_1.0_word_counts_train.json'),
 		os.path.join(dir_path, 'visdial_1.0_val_dense_annotations.json'),
-		os.path.join(dir_path, 'visdial_1.0_train.json'),
 		os.path.join(dir_path, 'visdial_1.0_val.json'),
+		os.path.join(dir_path, 'visdial_1.0_train.json'),
+		os.path.join(dir_path, 'features_faster_rcnn_x101_train.h5'),
 		]
 
 	file_links = [
-		'https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_train.h5',
 		'https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_val.h5',
 		'https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_test.h5',
 		'https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/visdial_1.0_word_counts_train.json',
 		'https://www.dropbox.com/s/3knyk09ko4xekmc/visdial_1.0_val_dense_annotations.json?dl=0',
-		'https://www.dropbox.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip?dl=0',
 		'https://www.dropbox.com/s/ibs3a0zhw74zisc/visdial_1.0_val.zip?dl=0',
+		'https://www.dropbox.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip?dl=0',
+		'https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_train.h5',
 		]
 
+	if train is False:
+		file_paths == file_paths[:-2]
+
 	for i in range(len(file_paths)):
-		if train is False and i == 0:
-			continue
 
 		if not file_exists(file_paths[i]):
 			print('Downloading {} ...'.format(file_paths[i]))
 			if not file_exists(os.path.dirname(file_paths[i])):
 				os.makedirs(os.path.dirname(file_paths[i]))
 
+			if 'zip' in file_links[i]:
+				file_paths[i] = '.'.join(file_paths[i].split('.')[:-1].append('zip'))
 			execute_cmd('wget {} --output-document={}'.format(file_links[i], file_paths[i]))
 			print('Finished downloading!\n')
 
-	for file in file_paths[-2:]:
-		execute_cmd('unzip {}'.format(file))
+	for file in file_paths:
+		if 'zip' in file:
+			execute_cmd('unzip {}'.format(file))
+			execute_cmd('rm {}'.format(file))
+
