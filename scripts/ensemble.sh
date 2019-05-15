d2476c5186351063d79f5b958975c8a41125ab46
Quang-Mac <quangdtsc@gmail.com>
2019-05-15 21:59:07 +0200

    colab

--- scripts/ensemble.sh
+++ scripts/ensemble.sh
@@ -15,7 +15,7 @@ python $PATH_PROJ/ensemble.py \
 --json-dense $DATA/visdial_1.0_val_dense_annotations.json \
 --load-pthpaths $CKPT/checkpoint_best_mean.pth $CKPT/checkpoint_best_ndcg.pth \
 --image-features-h5 $DATA/features_faster_rcnn_x101_val.h5 \
---save-ranks-path $CKPT/val_rank.json \
+--save-ranks-path log/val_rank.json \
 --json-word-counts $DATA/visdial_1.0_word_counts_train.json
 
 
