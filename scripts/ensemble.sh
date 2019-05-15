934922ed51cc24cc1eb4ac47beabb07655d96f9d
Quang-Mac <quangdtsc@gmail.com>
2019-05-15 23:27:20 +0200

    colab

--- scripts/ensemble.sh
+++ scripts/ensemble.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-SPLIT='val'
-ROOT='/content'
-PATH_PROJ=$ROOT/visdial
-DATA=$ROOT/datasets/visdial
-CONFIG=$PATH_PROJ/configs/lf_disc_faster_rcnn_x101.yml
-
-python $PATH_PROJ/ensemble.py \
---split val \
---json-dialogs $DATA/visdial_1.0_val.json \
---json-dense $DATA/visdial_1.0_val_dense_annotations.json \
---image-features-h5 $DATA/features_faster_rcnn_x101_val.h5 \
---json-word-counts $DATA/visdial_1.0_word_counts_train.json
+#SPLIT='val'
+#ROOT='/content'
+#PATH_PROJ=$ROOT/visdial
+#DATA=$ROOT/datasets/visdial
+#CONFIG=$PATH_PROJ/configs/lf_disc_faster_rcnn_x101.yml
+#
+#python $PATH_PROJ/ensemble.py \
+#--split val \
+#--json-dialogs $DATA/visdial_1.0_val.json \
+#--json-dense $DATA/visdial_1.0_val_dense_annotations.json \
+#--image-features-h5 $DATA/features_faster_rcnn_x101_val.h5 \
+#--json-word-counts $DATA/visdial_1.0_word_counts_train.json
 
 
 #!/usr/bin/env bash
-
 SPLIT='test'
 ROOT='/content'
 PATH_PROJ=$ROOT/visdial
