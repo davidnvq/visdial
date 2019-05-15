c32a86cefbe8a7adefccd95812a593ec5e396047
Quang-Mac <quangdtsc@gmail.com>
2019-05-15 23:47:20 +0200

    colab

--- scripts/ensemble.sh
+++ scripts/ensemble.sh
@@ -22,7 +22,7 @@ DATA=$ROOT/datasets/visdial
 CONFIG=$PATH_PROJ/configs/lf_disc_faster_rcnn_x101.yml
 
 python $PATH_PROJ/ensemble.py \
---split val \
+--split $SPLIT \
 --json-dialogs $DATA/visdial_1.0_test.json \
 --image-features-h5 $DATA/features_faster_rcnn_x101_test.h5 \
 --json-word-counts $DATA/visdial_1.0_word_counts_train.json
