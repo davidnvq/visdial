941d8479af9bcf2e6137b4df15788575b9088228
Quang-Mac <quangdtsc@gmail.com>
2019-05-15 22:04:46 +0200

    colab

--- scripts/ensemble.sh
+++ scripts/ensemble.sh
@@ -4,7 +4,8 @@ SPLIT='val'
 ROOT='/content'
 PATH_PROJ=$ROOT/visdial
 DATA=$ROOT/datasets/visdial
-CKPT='/content/gdrive/My Drive/checkpoints/lf_disc/may13'
+CKPT1='/content/gdrive/My\ Drive/checkpoints/lf_disc/may13/checkpoint_best_ndcg.pth'
+CKPT2='/content/gdrive/My\ Drive/checkpoints/lf_disc/may13/checkpoint_best_mean.pth'
 CONFIG=$PATH_PROJ/configs/lf_disc_faster_rcnn_x101.yml
 
 python $PATH_PROJ/ensemble.py \
@@ -13,7 +14,7 @@ python $PATH_PROJ/ensemble.py \
 --config-ymls $CONFIG $CONFIG \
 --json-dialogs $DATA/visdial_1.0_val.json \
 --json-dense $DATA/visdial_1.0_val_dense_annotations.json \
---load-pthpaths $CKPT/checkpoint_best_mean.pth $CKPT/checkpoint_best_ndcg.pth \
+--load-pthpaths $CKPT1 $CKPT2 \
 --image-features-h5 $DATA/features_faster_rcnn_x101_val.h5 \
 --save-ranks-path log/val_rank.json \
 --json-word-counts $DATA/visdial_1.0_word_counts_train.json
